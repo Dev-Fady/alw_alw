f86f0251d5415c09ea318fd49bae0f19b98932fa
fady emil <fadymobiledev@gmail.com>
2025-03-07 11:35:24 +0200

    add ZegoService for user login and logout handling

---
+++ lib/core/services/zego_service/zego_service.dart
@@ -0,0 +1,26 @@
+import 'package:alw_alw/core/constants/zego_cloud.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+
+class ZegoService {
+  /// on App's user login
+  void onUserLogin({required String userID, required String userName}) {
+    /// 1.2.1. initialized ZegoUIKitPrebuiltCallInvitationService
+    /// when app's user is logged in or re-logged in
+    /// We recommend calling this method as soon as the user logs in to your app.
+    ZegoUIKitPrebuiltCallInvitationService().init(
+      appID: ZegoCloud.appID /*input your AppID*/,
+      appSign: ZegoCloud.appSign /*input your AppSign*/,
+      userID: userID,
+      userName: userName,
+      plugins: [ZegoUIKitSignalingPlugin()],
+    );
+  }
+
+  /// on App's user logout
+  void onUserLogout() {
+    /// 1.2.2. de-initialization ZegoUIKitPrebuiltCallInvitationService
+    /// when app's user is logged out
+    ZegoUIKitPrebuiltCallInvitationService().uninit();
+  }
+}
