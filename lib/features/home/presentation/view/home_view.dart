e228b8768d72e441dc2adf89cf3c1a3f041b348d
fady emil <fadymobiledev@gmail.com>
2025-03-07 11:35:40 +0200

    add ZegoSendCallInvitationButton to HomeView for video call functionality

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,6 +11,28 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ZegoSendCallInvitationButton(
+              isVideoCall: true,
+              //You need to use the resourceID that you created in the subsequent steps.
+              //Please continue reading this document.
+              resourceID: "zegouikit_call",
+              invitees: [
+                ZegoUIKitUser(
+                  id: '12345',
+                  name: 'fady',
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
