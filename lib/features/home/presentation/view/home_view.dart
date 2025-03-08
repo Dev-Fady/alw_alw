c0f01bb49cfcf8a96f167d008416a7dcc0b278e0
fady emil <fadymobiledev@gmail.com>
2025-03-08 18:40:27 +0200

    add form for user input in HomeView and integrate ZegoService for call functionality

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:alw_alw/core/cache/cache_helper.dart';
+import 'package:alw_alw/core/services/zego_service/zego_service.dart';
 import 'package:flutter/material.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
 
@@ -9,24 +11,89 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _idController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  ZegoService zegoService = ZegoService();
+  CacheHelper prefs = CacheHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    zegoService.onUserLogin(
+      userID: prefs.getData(key: 'id'),
+      userName: prefs.getData(key: 'userName'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: const Icon(Icons.person),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your name';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    controller: _idController,
+                    decoration: InputDecoration(
+                      labelText: 'ID',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: const Icon(Icons.credit_card),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your ID';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+              },
+              child: const Text('Call'),
+            ),
             ZegoSendCallInvitationButton(
-              isVideoCall: true,
-              //You need to use the resourceID that you created in the subsequent steps.
-              //Please continue reading this document.
-              resourceID: "zegouikit_call",
+              isVideoCall: false,
+              resourceID: "alw-alw",
               invitees: [
                 ZegoUIKitUser(
-                  id: '12345',
-                  name: 'fady',
+                  id: _idController.text.trim(),
+                  name: _nameController.text.trim(),
                 ),
               ],
             )
