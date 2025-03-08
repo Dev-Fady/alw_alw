186c849d71e5bd6d4e420b4f1def9b2314db4649
fady emil <fadymobiledev@gmail.com>
2025-03-08 19:28:57 +0200

    refactor HomeView to implement logout functionality and separate body into BodyHomeView widget

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,7 +1,9 @@
 import 'package:alw_alw/core/cache/cache_helper.dart';
 import 'package:alw_alw/core/services/zego_service/zego_service.dart';
+import 'package:alw_alw/features/home/presentation/view/widget/body_home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+import '../../../login/presentation/login_view.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -11,10 +13,6 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _idController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-
   ZegoService zegoService = ZegoService();
   CacheHelper prefs = CacheHelper();
 
@@ -27,6 +25,13 @@ class _HomeViewState extends State<HomeView> {
     );
   }
 
+  void _logout(BuildContext context) {
+    prefs.clearData();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => LoginView()));
+    zegoService.onUserLogout();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,72 +39,16 @@ class _HomeViewState extends State<HomeView> {
         title: const Text('Home'),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              _logout(context);
+            },
+          )
+        ],
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      labelText: 'Name',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      prefixIcon: const Icon(Icons.person),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your name';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormField(
-                    controller: _idController,
-                    decoration: InputDecoration(
-                      labelText: 'ID',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      prefixIcon: const Icon(Icons.credit_card),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your ID';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {});
-              },
-              child: const Text('Call'),
-            ),
-            ZegoSendCallInvitationButton(
-              isVideoCall: false,
-              resourceID: "alw-alw",
-              invitees: [
-                ZegoUIKitUser(
-                  id: _idController.text.trim(),
-                  name: _nameController.text.trim(),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      body: BodyHomeView(),
     );
   }
 }
