13336d428a0f088934e4b36613db0e03c0c96012
fady emil <fadymobiledev@gmail.com>
2025-03-11 18:33:56 +0200

    update ButtonLogin to navigate to MainView after successful login

--- lib/features/login/presentation/widget/button_login.dart
+++ lib/features/login/presentation/widget/button_login.dart
@@ -1,6 +1,6 @@
 import 'package:alw_alw/core/cache/cache_helper.dart';
 import 'package:alw_alw/core/services/zego_service/zego_service.dart';
-import 'package:alw_alw/features/add_pepole/presentation/view/add_pepole_view.dart';
+import 'package:alw_alw/features/main/presentation/view/main_view.dart';
 // import 'package:alw_alw/features/home/presentation/view/home_view.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +11,9 @@ class ButtonLogin extends StatelessWidget {
     required this.prefs,
     required TextEditingController idController,
     required TextEditingController nameController,
-  }) : _formKey = formKey, _idController = idController, _nameController = nameController;
+  })  : _formKey = formKey,
+        _idController = idController,
+        _nameController = nameController;
 
   final GlobalKey<FormState> _formKey;
   final CacheHelper prefs;
@@ -27,18 +29,16 @@ class ButtonLogin extends StatelessWidget {
         onPressed: () async {
           if (_formKey.currentState!.validate()) {
             prefs.saveData(key: 'id', value: _idController.text);
-            prefs.saveData(
-                key: 'userName', value: _nameController.text);
+            prefs.saveData(key: 'userName', value: _nameController.text);
             ZegoService zegoService = ZegoService();
             zegoService.onUserLogin(
               userID: _idController.text,
               userName: _nameController.text,
             );
-            await CacheHelper()
-                .saveData(key: 'isLoggedIn', value: true);
+            await CacheHelper().saveData(key: 'isLoggedIn', value: true);
             Navigator.of(context).pushReplacement(MaterialPageRoute(
               builder: (context) {
-                return AddPepoleView();
+                return MainView();
               },
             ));
           }
