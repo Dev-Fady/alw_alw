14d2b6cc1f32851295f39aeae5c2edfe031510bb
fady emil <fadymobiledev@gmail.com>
2025-03-11 17:14:23 +0200

    add HomeRepo interface for user data retrieval

---
+++ lib/features/home/domain/repo/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:alw_alw/core/error/faliure.dart';
+import 'package:alw_alw/features/add_pepole/domain/entites/add_pepole_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Faliure, List<AddPepoleEntity>>> getUser();
+}
